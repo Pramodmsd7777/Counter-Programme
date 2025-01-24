cb4e78b5229843629168226cfc3af9f02016c5b8
Pramod Reddy Yekkanti <yekkantipramodreddy7777@gmail.com>
2025-01-24 13:01:25 +0530

    Add files via upload
    This is a counter program that is used to increment and decrement numbers as we need.

---
+++ App.jsx
@@ -0,0 +1,16 @@
+
+// import  Student from './Student.jsx'
+// import UserGreeting from './UserGreeting.jsx';
+import Profile from './Profile.jsx';
+import Button from './Button.jsx';
+// import MyComponent from './MyComponent.jsx';
+import Counter from './Counter.jsx';
+function App(){
+ return(
+      <>
+        <Counter/> 
+      </>
+  );
+}
+
+export default App
